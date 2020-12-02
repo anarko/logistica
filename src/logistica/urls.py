cba5c82878e0ae1badb188271a69d9e0c5cf6d0e
anarko <anarko@anarka>
2020-12-02 06:25:30 -0300

    changed app url

--- src/logistica/urls.py
+++ src/logistica/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('logistica/', include('recupero.urls')),
+    path('recupero/', include('recupero.urls')),
     path('admin/', admin.site.urls),
 ]
